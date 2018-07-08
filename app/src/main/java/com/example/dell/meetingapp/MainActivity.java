ba377caa886f48185a701f5991219d9166c1772b
raspreet kaur <kaurraspreet125@gmail.com>
2018-07-07 21:19:34 -0700

    layout is done with splash screen

---
+++ app/src/main/java/com/example/dell/meetingapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.dell.meetingapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+     ImageButton addBtn,viewBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addBtn=(ImageButton)findViewById(R.id.imageButton);
+        viewBtn=(ImageButton)findViewById(R.id.imageButton2);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Add.class);
+                startActivity(i);
+            }
+        });
+        viewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,view.class);
+                startActivity(i);
+            }
+        });
+    }
+}
